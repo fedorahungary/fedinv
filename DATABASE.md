03b452480a51a0f224c582a7e0b2a5d797872cf7
Levente Kurusa <levex@linux.com>
2014-11-16 14:18:03 +0100

    db: use sqlite3, add docs, add table 'swag_types'
    Signed-off-by: Levente Kurusa <levex@linux.com>

---
+++ DATABASE.md
@@ -0,0 +1,13 @@
+Database Specification
+==================
+
+Database file: db_default
+
+Tables
+=====
+
+* swag_types
+  * id (Integer) - PK for the table
+  * name (Text) - Name of swagtype in English (US)
+  * orderable (Text) - Can more of this ordered? (Values: 'true' / 'false')
+  * amount (Integer) - Total amount at Ambassadors
--- fedinv/fedinv/settings.py
+++ fedinv/fedinv/settings.py
@@ -4,15 +4,15 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+     ('Levente Kurusa', 'levex@fedoraproject.org'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'db_default',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
