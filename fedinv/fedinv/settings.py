0d18ae52ffb85c20fd8127fc8315ce581d840900
Levente Kurusa <levex@linux.com>
2014-11-23 12:42:02 +0100

    templates: move templates to fedinv/
    Signed-off-by: Levente Kurusa <levex@linux.com>

--- fedinv/fedinv/settings.py
+++ fedinv/fedinv/settings.py
@@ -111,7 +111,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "swag_reports/templates",
+    "fedinv/templates",
 )
 
 INSTALLED_APPS = (
